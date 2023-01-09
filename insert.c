fb1f177614c7b15b2e29f7f73dfbed37d1a47b0b
mathinfodz <122266980+mathinfodz@users.noreply.github.com>
2023-01-09 21:06:21 +0100

    Update insert.c

--- insert.c
+++ insert.c
@@ -5,6 +5,46 @@ struct Node{
     int data;
     struct Node *next;
 };
+int calcSize(struct Node* node){
+    int size=0;
+
+    while(node!=NULL){
+        node = node->next;
+        size++;
+    }
+    return size;
+}
+
+void insertPosition(int pos, int data, struct Node** head)
+{
+    int size = calcSize(*head);
+
+    //If pos is 0 then should use insertStart method
+    //If pos is less than 0 then can't enter at all
+    //If pos is greater than size then bufferbound issue
+    if(pos < 1 || size < pos) 
+    { 
+        printf("Can't insert, %d is not a valid position\n",pos); 
+    } 
+    else 
+    { 
+        struct Node* temp = *head; 
+        struct Node* newNode = (struct Node*) 
+        malloc(sizeof(struct Node)); 
+        newNode->data = data;
+        newNode->next = NULL;
+
+        while(--pos)
+        {
+            temp=temp->next;
+        }
+        //(25)->next = 10 as 12->next is 10 
+        newNode->next= temp->next;
+        // (12)->next = 25
+        temp->next = newNode;
+        //new node added in b/w 12 and 10
+    }
+}
 
 void insertStart(struct Node** head, int data){
 
@@ -50,43 +90,21 @@ void display(struct Node* node){
 
 int main()
 {
-    
-    //creating 4 pointers of type struct Node
-    //So these can point to address of struct type variable
-    struct Node* head = NULL; 
-    struct Node* node2 = NULL; 
-    struct Node* node3 = NULL; 
-    struct Node* node4 = NULL; 
-
-    // allocate 3 nodes in the heap 
-    head =  (struct Node*)malloc(sizeof(struct Node)); 
-    node2 = (struct Node*)malloc(sizeof(struct Node)); 
-    node3 = (struct Node*)malloc(sizeof(struct Node)); 
-    node4 = (struct Node*)malloc(sizeof(struct Node)); 
-
-   
-    head->data = 15; // data set for head node 
-    head->next = node2; // next pointer assigned to address of node2 
-
-    node2->data = 10; 
-    node2->next = node3; 
-
-    node3->data = 12;
-    node3->next = node4; 
-
-    node4->data = 3;
-    node4->next = NULL;
-    
-    printf("Linklist : ");
-    display(head);
+    struct Node* head = NULL;
 
     // Need '&' i.e. address as we need to change head
-    insertStart(&head,25);
-    
+    insertStart(&head,12);
+    insertStart(&head,16);
+    insertStart(&head,20);
+
+    insertLast(&head,10);
+    insertLast(&head,14);
+    insertLast(&head,18);
     insertLast(&head,11);
     
-    printf("\nAfter Inserting Element\n");
-    printf("\nLinklist : ");
+    //Inserts after 3rd position
+    insertPosition(3,25,&head);
+    
     // no need for '&' as head need not be changed
     // only doing traversal
     display(head); 
